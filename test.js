1993f2dfee90c46e33a08c89a4b90398d98011d1
Harshit Mishra <harshitlove28@outlook.com>
2023-09-28 18:04:44 +0530

    cdn Type static viewing to share the files in original form

--- test.js
+++ test.js
@@ -14,7 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 // Set up view engine and views folder
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
-
+// server uploads as static files
+app.use('/uploads', express.static('uploads'));
 // Serve the upload form
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -30,17 +31,17 @@ app.post('/upload', (req, res) => {
       console.error(err);
       return res.status(500).send('An error occurred');
     }
-
+    
+    var fileUrl = generateRandomID()+files.file[0].originalFilename;
     var oldPath = files.file[0].filepath;
-    var newPath = path.join(form.uploadDir, files.file[0].originalFilename);
-
+    var newPath = path.join(form.uploadDir,fileUrl);
     fs.rename(oldPath, newPath, (err) => {
       if (err) {
         console.error(err);
         return res.status(500).send('An error occurred');
       }
         // res.send('File uploaded successfully');
-      res.render('result', { path: newPath });
+      res.render('result', { path: newPath,url:fileUrl });
     });
 
   });
@@ -49,3 +50,15 @@ module.exports = app; // Export the Express app
 // app.listen(port, () => {
 //   console.log(`Server listening on port ${port}`);
 // });
+
+const generateRandomID = () => {
+  let id = ``;
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < 5; i++) {
+    id += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  id += '-';
+  return id;
+};
